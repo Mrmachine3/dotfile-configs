716d21d86c8720ddeca268b84a8dd2e3aefff53d
Mr.Machine <34361741+Mrmachine3@users.noreply.github.com>
2018-10-03 20:33:49 -0500

    Update wifi-rebooter.sh
    Updated script metadata

--- wifi-rebooter.sh
+++ wifi-rebooter.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#---Author: Anthony Rodriguez
+#---Created Date: 10/03/2018
+#---Project Tag: linux, utility, bash
+#---Purpose:
+#   The purpose of this script is to restart the wireless interface.
+#   Additionally, this script is added as a cron job to run every 5 minutes.
+#--->
 
 # The IP for the server you wish to ping (8.8.8.8 is a public Google DNS server)
 SERVER=8.8.8.8
