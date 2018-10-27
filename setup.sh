134032f4d5ed4ec10e1930f65c3f26917db3d9e0
mrmachine <mrmachine@localhost.localdomain>
2018-10-27 01:34:07 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@
 #---Purpose:
 #   The purpose of this script is to setup the current machine
 #   with preferences, including the following:
-#    - Adding mrmachine user to sudoer file
 #    - package management maintenance
 #    - change default shell to zsh
 #    - implement oh-my-zsh shell custom preferences
@@ -15,20 +14,20 @@
 # Defining variables
 #user=$(whoami)
 #PASSWD=123456
-#ROOTPASSWD=987654
 
 # Adding new sudoer
 #useradd $USER
 #passwd $PASSWD
 #su root; echo "$ROOTPASSWD" && sudo echo "${user}" ALL=(ALL)       ALL' | sudo tee -a ~/Desktop/sample_sudoer_file.txt
 
-
 # Update and upgrade package lists
 #apt-get update && apt-get upgrade && apt-get check && apt-get -f install
+echo "DONE...Completed package manager update and upgrade process"
 
 # Install packages and clean up unused packages 
-apt-get install curl git hub 
+apt-get install curl git hub zsh
 apt-get autoclean && apt-get autoremove
+echo "DONE...installed specified packages"
 
 # Add zsh to /etc/shells
 command -v zsh | sudo tee -a /etc/shells && cat /etc/shells
@@ -36,18 +35,25 @@ echo "DONE...Added $(command -v zsh) to /etc/shells"
 
 # User chsh to set the shell to zsh
 sudo chsh -s "$(command -v zsh)" "${USER}"
-echo "DONE...Changed default shell to zsh"
+echo "DONE...Changed default shell for $USER"
+echo "DONE...Current default shell: $SHELL"
 
 # Change working directory to home directory and install oh-my-zsh from github
-cd; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"; echo "exit"
-echo "DONE...Finished installation of oh-my-zsh"
+if [ -d ~/.oh-my-zsh ]
+then 
+    echo "Oh-My-Zsh has already been installed"
+else
+    cd; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    echo "DONE...Finished installation of OMZ"
+fi
 
 # execute git clone of mrmachine3/preferences
 cd; git clone https://github.com/mrmachine3/preferences.git
 echo "DONE...Finished cloning of Mr. Machine's preferences"
 
-# check if original .zshrc file exists and save copy of dotfile by appending .orig to filename
+# check if original .zshrc/.bashrc files exist and save copy of dotfile by appending .orig to filename
 file1=".zshrc"
+file2=".bashrc"
 if [ -e "$file1" ]
 then
     echo "$file1 found!" && mv ~/.zshrc ~/.zshrc.orig && cp ~/preferences/.zshrc_master ~/.zshrc
@@ -56,10 +62,9 @@ else
 fi
 
 # check if original .bashrc file exists and save a copy of dotfile by appending .orig to filename
-file2=".bashrc"
 if [ -e "$file2" ]
 then 
-    echo "$file2 found!" && mv ~/.bashrc ~/.bashrc.orig && cp ~/preferences/.zshrc_master ~/.zshrc
+    echo "$file2 found!" && mv ~/.bashrc ~/.bashrc.orig
 else
     echo "$file2 not found" && cp ~/preferences/.zshrc_master ~/.zshrc
 fi
