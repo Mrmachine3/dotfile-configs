e4b89b9cbaf0fe324afcaa232d4b0bf35d2e06db
mrmachine <mrmachine@localhost.localdomain>
2018-10-27 00:25:19 -0500

    updated setup script

--- setup.sh
+++ setup.sh
@@ -24,23 +24,27 @@
 
 
 # Update and upgrade package lists
-apt-get update && apt-get upgrade && apt-get check && apt-get -f install
+#apt-get update && apt-get upgrade && apt-get check && apt-get -f install
 
 # Install packages and clean up unused packages 
-#apt-get install curl git hub 
-#apt-get autoclean && apt-get autoremove
+apt-get install curl git hub 
+apt-get autoclean && apt-get autoremove
 
 # Add zsh to /etc/shells
-command -v zsh | sudo tee -a /etc/shells
+command -v zsh | sudo tee -a /etc/shells && cat /etc/shells
+echo "DONE...Added $(command -v zsh) to /etc/shells"
 
 # User chsh to set the shell to zsh
 sudo chsh -s "$(command -v zsh)" "${USER}"
+echo "DONE...Changed default shell to zsh"
 
 # Change working directory to home directory and install oh-my-zsh from github
 cd; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"; echo "exit"
+echo "DONE...Finished installation of oh-my-zsh"
 
 # execute git clone of mrmachine3/preferences
 cd; git clone https://github.com/mrmachine3/preferences.git
+echo "DONE...Finished cloning of Mr. Machine's preferences"
 
 # check if original .zshrc file exists and save copy of dotfile by appending .orig to filename
 file1=".zshrc"
@@ -58,10 +62,12 @@ then
     echo "$file2 found!" && mv ~/.bashrc ~/.bashrc.orig && cp ~/preferences/.zshrc_master ~/.zshrc
 else
     echo "$file2 not found" && cp ~/preferences/.zshrc_master ~/.zshrc
+fi
 
 # move theme file to .oh-my-zsh themes folder
-cd ~/preferences; cp ./linux_machinemode.zsh-theme ~/oh-my-zsh/themes/machinemode.zsh-theme
-cd; source ./.zshrc
+cd && cp ~/preferences/linux_machinemode.zsh-theme ~/.oh-my-zsh/themes/machinemode.zsh-theme
+echo "DONE...Custom .zsh theme copied into .oh-my-zsh/theme directory"
+cd && source ./.zshrc
 
 #TO DO LIST
 # MOVE OR CREATE SYMLINK TO MACHINEMODE-THEME FILE (FROM PREFERENCES GIT REPOSITORY) INTO THE THEMES FOLDER OF OH-MY-ZSH DIRECTORY
