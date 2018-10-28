dcf15b6393886f2d98909ecaba4da2666cf7dfa7
mrmachine <mrmachine@localhost.localdomain>
2018-10-28 00:58:52 -0500

    updated .zshrc_master file and dirmaker.sh

--- dirmaker.sh
+++ dirmaker.sh
@@ -5,49 +5,65 @@
 #    │   ├── QTR
 #    │   │   ├─── COURSE
 #    │   │   │   ├── ABOUT.md
-#    │   │   │   ├── !ADMIN
+#    │   │   │   ├── admin
 #    │   │   │   ├── archive
 #    │   │   │   ├── research
-#    │   │   │   ├── images
 #    │   │   │   ├── notes
-#    │   │   │   ├── flashcards
+#    │   │   │   │   ├── images
+#    │   │   │   │   ├── flashcards
 #    │   │   │   ├── homework
 
 echo -n "Enter name of base directory: "
 read BASE
-echo -n "Enter year of subdirectory: "
+echo -n "Enter year: "
 read YEAR
-echo -n "Enter academic quarter of subdirectory: "
+echo -n "Enter academic quarter: "
 read QTR
 echo -n "Enter course #: "
 read COURSE
 echo -n "Enter course title: "
 read TITLE
+sleep 1
 
-sleep 2
+# Defining directory filepath
+DIR="$BASE"/"$YEAR"/"$QTR"/"$COURSE"
 
 # Creating base directory structure
-echo "Creating $BASE directory structure..." && sleep 1
-mkdir -p "$BASE"/"$YEAR"/"$QTR"/"$COURSE"
-sleep 1
+echo "Creating $BASE directory structure..."
+mkdir -p "$DIR" && sleep 1
 
 # Creating course intro about markdown file within course subdirectory
-cd "$BASE"/"$YEAR"/"$QTR"/"$COURSE" 
+cd "$DIR"
 touch ABOUT.md
-echo " $COURSE - $TITLE" >> ABOUT.md
-echo "[INSERT FILE HEADER]" >> ABOUT.md
-echo '"Created date: $(date +%m-%d-%Y)"'>> ABOUT.md
-echo "Created $TITLE markdown file located in $COURSE subdirectory"
 sleep 1
+echo "$COURSE - $TITLE" >> ABOUT.md
+echo "Professor " >> ABOUT.md
+echo "Created date: $(date +%m-%d-%Y)" >> ABOUT.md
+echo "Created by: Mr. Machine" >> ABOUT.md
+echo "Created $TITLE markdown file located in $COURSE subdirectory"
+sleep 3
 
 # Creating subdirectories in course folder
-echo "Changing directory to $BASE/$YEAR/$QTR/$COURSE..."
-cd "$BASE"/"$YEAR"/"$QTR"/"$COURSE"
-mkdir -p ./{ADMIN,archive,research, notes}
+cd "$DIR"
+mkdir admin
+mkdir archive
+mkdir research
+mkdir notes
 echo "Created subdirectories in $COURSE folder..."
-sleep 2
+echo "tree -aC $PWD"
+sleep 5
+
+# Create subdirectories in notes subfolder
+cd "$DIR"/notes
+echo "tree -aC $PWD"
+sleep 3
+mkdir flashcards
+mkdir images
+echo "Created flashcards and images folder in notes folder..."
+sleep 1
 
 # command line feedback on directory tree generation
+cd ~/Desktop/$BASE
 tree -aC $BASE
 echo " "
 echo "$BASE directory created on" $(date +%m-%d-%Y)
