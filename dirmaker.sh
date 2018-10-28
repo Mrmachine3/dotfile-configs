129d89bfe26c5157a89b9c89c3fc45a197496383
mrmachine <mrmachine@localhost.localdomain>
2018-10-27 23:15:48 -0500

    updated dirmaker to be more verbose

--- dirmaker.sh
+++ dirmaker.sh
@@ -15,17 +15,40 @@
 
 echo -n "Enter name of base directory: "
 read BASE
+echo -n "Enter year of subdirectory: "
+read YEAR
+echo -n "Enter academic quarter of subdirectory: "
+read QTR
+echo -n "Enter course #: "
+read COURSE
+echo -n "Enter course title: "
+read TITLE
+
+sleep 2
+
+# Creating base directory structure
+echo "Creating $BASE directory structure..." && sleep 1
+mkdir -p "$BASE"/"$YEAR"/"$QTR"/"$COURSE"
 sleep 1
 
-YEAR="2019"
-QTR="WI'19"
-COURSE="TDC411"
-# COURSEDIR=ABOUT.md,!ADMIN,archive,research,$NOTESDIR,homework
-# NOTESDIR=notes/{images,flashcards}
-
-echo "Creating $BASE directory structure..."
+# Creating course intro about markdown file within course subdirectory
+cd "$BASE"/"$YEAR"/"$QTR"/"$COURSE" 
+touch ABOUT.md
+echo " $COURSE - $TITLE" >> ABOUT.md
+echo "[INSERT FILE HEADER]" >> ABOUT.md
+echo '"Created date: $(date +%m-%d-%Y)"'>> ABOUT.md
+echo "Created $TITLE markdown file located in $COURSE subdirectory"
 sleep 1
 
-mkdir -pv "$BASE"/$YEAR/$QTR/$COURSE/{ABOUT.md,!ADMIN,archive,research,notes/{images,flashcards},homework
-sleep 2 && echo "$BASE directory created on" $(date +%m-%d-%Y) 
-sleep 1 && tree -aC $BASE
+# Creating subdirectories in course folder
+echo "Changing directory to $BASE/$YEAR/$QTR/$COURSE..."
+cd "$BASE"/"$YEAR"/"$QTR"/"$COURSE"
+mkdir -p ./{ADMIN,archive,research, notes}
+echo "Created subdirectories in $COURSE folder..."
+sleep 2
+
+# command line feedback on directory tree generation
+tree -aC $BASE
+echo " "
+echo "$BASE directory created on" $(date +%m-%d-%Y)
+echo "DONE...dirmaker.sh script was completed successfully"
